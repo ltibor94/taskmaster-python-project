315dc9647e0266b5245cda758c4dde3fa8ad2688
Tibor <l94tibor@gmail.com>
2025-06-09 14:53:05 +0300

    Fixing minor issues

--- controller/task_controller.py
+++ controller/task_controller.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from flask import Blueprint, redirect, render_template, request
 from dto.update_task_request_dto import UpdateTaskRequestDto
 from logger.logger import Logger
@@ -6,15 +7,16 @@ from service.task_service import TaskService
 task_blueprint = Blueprint('tasks', __name__)
 task_service = TaskService()
 
-@task_blueprint.route("/", methods=['GET', 'POST'])
+@task_blueprint.route("/", methods=['GET', 'POST']) # type: ignore
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
         try:
             task_service.create_task(task_content)
             return redirect('/')
-        except:
-            return 'There was a problem.'
+        except Exception as e:
+            render_template('error.html', 
+                            error_message = f'There was a problem new tasks: {e}')
     else:
         try:
             tasks = task_service.get_all_tasks()
