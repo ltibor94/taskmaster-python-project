98047f71b9bad24f8238191d0e22d0d7f03fadcc
Tibor <l94tibor@gmail.com>
2025-06-09 11:36:50 +0300

    Adding Bootsrap design

--- controller/task_controller.py
+++ controller/task_controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, redirect, render_template, request
+from dto.update_task_request_dto import UpdateTaskRequestDto
 from logger.logger import Logger
 from service.task_service import TaskService
 
@@ -19,7 +20,8 @@ def index():
             tasks = task_service.get_all_tasks()
             return render_template('index.html', tasks=tasks)
         except Exception as e:
-            return render_template('index.html')
+            return render_template('error.html', 
+                            error_message = f'There was a problem retrieving tasks: {e}')
 
 @task_blueprint.route('/delete/<int:id>')
 def delete(id: int):
@@ -27,15 +29,23 @@ def delete(id: int):
         task_service.delete_task(id)
         return redirect('/')
     except:
-        return 'There was a problem deleting that task.'
+        return render_template('error.html', 
+                            error_message = 'There was a problem deleting that task.')
 
 @task_blueprint.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id: int):
+    logger = Logger("TaskController").get_logger()
+    logger.info(f"Updating task with ID: {id}")
     if request.method == 'POST':
         try:
-            task_service.update_task(id, request.form['content'])
+            completed = request.form.get('completed') == 'on'
+            update_request = UpdateTaskRequestDto(request.form['content'], completed)
+            logger.info(f"Update request: {update_request}")
+            task_service.update_task(id, update_request)
             return redirect('/')
-        except:
-            return 'There was a problem updating that task.'
+        except Exception as e:
+            logger.error(f"Error updating task with ID {id}: {e}, request: {request.form}")
+            return render_template('error.html', 
+                                   error_message = 'There was a problem updating that task.')
     else:
         return render_template('update.html', task=task_service.get_task(id))
